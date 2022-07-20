4b02eaf70df3cea418837cfaab46e688f1a280f0
Michele Allison Joseph <michelejoseph@fb.com>
2022-07-20 14:49:13 -0700

    fix error

--- app/src/main/java/com/example/cocktailme/models/Cocktails.java
+++ app/src/main/java/com/example/cocktailme/models/Cocktails.java
@@ -29,7 +29,7 @@ public class Cocktails {
         cocktailPath = jsonObject.getString("strDrinkThumb");
         cocktailID = jsonObject.getInt("idDrink");
     }
-    public int getAverageRating() {
+    public double getAverageRating() {
         List<Rating> ratings = new ArrayList<Rating>();
         int total = 0;
         for (int i = 0; i < ratings.size(); ++i) {
